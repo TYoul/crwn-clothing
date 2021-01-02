a57e081693de6be2a8ae2fccd7e0488a12cf8088
chenzhouxu <710916361@qq.com>
2021-01-03 01:12:40 +0800

    创建cart-item组件，把购物车数据连接到redux中

---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,19 @@
+import React, { memo } from 'react';
+
+import './cart-item.style.scss';
+
+export default memo(function CartItem({
+  item: { imageUrl, price, name, quantity },
+}) {
+  return (
+    <div className="cart-item">
+      <img src={imageUrl} alt="item" />
+      <div className="item-details">
+        <span className="name">{name}</span>
+        <span className="price">
+          {quantity} x ${price}
+        </span>
+      </div>
+    </div>
+  );
+});
