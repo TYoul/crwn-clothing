f7e8536cd61ce7b94031d733d6509fe7065ea42c
chenzhouxu <710916361@qq.com>
2021-01-08 18:02:00 +0800

    将目录和商店数据移动到其自己的reducer中，创建各自的选择器，并使用redux流更新目录/商店的相应组件

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,22 +1,17 @@
 import React from 'react';
-import {connect} from 'react-redux'
-import {createStructuredSelector} from "reselect";
-import {selectShopItems} from '../../redux/shop/shop.selector'
+import { Route } from 'react-router-dom';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import CollectionOverview from '../../components/collection-overview/collection-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-const ShopPage = ({collections}) => {
+const ShopPage = ({ match }) => {
+  console.log(match);
   return (
     <div className="shop-page">
-      {collections.map(({id, ...otherCollectionsProps}) => (
-        <CollectionPreview key={id} {...otherCollectionsProps} />
-      ))}
+      <Route exact path={`${match.path}`} component={CollectionOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
   );
-}
+};
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectShopItems
-})
-
-export default connect(mapStateToProps, null)(ShopPage)
+export default ShopPage;
